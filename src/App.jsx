e8aa8becb465c49ac2c40c8b559e9efacd1d6cb3
Sayan Snigdha Pal <sayanpalb@gmail.com>
2025-04-13 12:48:28 +0530

    few improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   Button,
@@ -11,6 +11,7 @@ import {
   AppBar,
   Box,
   Slide,
+  Tooltip,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
@@ -30,6 +31,7 @@ function App() {
   const [open, setOpen] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
   const [detailsOpen, setDetailsOpen] = useState(false);
+  const [currentTimePosition, setCurrentTimePosition] = useState(null);
 
   const handleClose = () => {
     setSelectedTask(null);
@@ -65,117 +67,140 @@ function App() {
     const [sHour, sMinute] = startTime24.split(":").map(Number);
     const [eHour, eMinute] = endTime24.split(":").map(Number);
 
-    const startOfDayHour = 0;
     const totalHoursInView = 24;
-    const totalMinutesInView = totalHoursInView * 60;
+    const minutesPerHour = 60;
+    const scheduleHeightVh = 70;
+    const slotHeightVh = scheduleHeightVh / totalHoursInView;
 
-    const startMinutesFromViewStart = (sHour - startOfDayHour) * 60 + sMinute;
-    const durationInMinutes = (eHour * 60 + eMinute) - (sHour * 60 + sMinute);
+    const top =
+      sHour * slotHeightVh + (sMinute / minutesPerHour) * slotHeightVh;
 
-    const topPercentage = (startMinutesFromViewStart / totalMinutesInView) * 100;
-    const heightPercentage = (durationInMinutes / totalMinutesInView) * 100;
-
-    const top = (topPercentage / 100) * 70;
-    const height = (heightPercentage / 100) * 70;
+    const durationHours = eHour - sHour;
+    const durationMinutes = eMinute - sMinute;
+    const totalDurationMinutes =
+      durationHours * minutesPerHour + durationMinutes;
+    const height = (totalDurationMinutes / minutesPerHour) * slotHeightVh;
 
     return (
-      <Box
+      <Tooltip
+        title={`${task.title}\n${task.description}\nFrom: ${task.startTime} - To: ${task.endTime}`}
         key={`${task.id}-${startTime24}-${endTime24}`}
-        className="task-block"
-        sx={{
-          position: "absolute",
-          left: 8,
-          right: 8,
-          background: taskColors[index % taskColors.length],
-          borderLeft: `4px solid ${
-            taskColors[((index % taskColors.length) * 2) % taskColors.length]
-          }`,
-          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-          padding: 1,
-          borderRadius: 4,
-          overflow: "hidden",
-          zIndex: 1,
-          transition: "transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out",
-          "&:hover": {
-            transform: "scale(1.01)",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.15)",
-          },
-          top: `${top}vh`,
-          height: `${height}vh`,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-        }}
       >
         <Box
+          className="task-block"
           sx={{
+            position: "absolute",
+            left: 8,
+            right: 8,
+            background: taskColors[index % taskColors.length],
+            borderLeft: `4px solid ${
+              taskColors[((index % taskColors.length) * 2) % taskColors.length]
+            }`,
+            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+            padding: 1,
+            borderRadius: 4,
+            overflow: "hidden",
+            zIndex: 1,
+            transition:
+              "transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out",
+            "&:hover": {
+              transform: "scale(1.01)",
+              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.15)",
+            },
+            top: `${top}vh`,
+            height: `${height}vh`,
             display: "flex",
+            flexDirection: "column",
             justifyContent: "space-between",
-            alignItems: "center",
-            mb: 1,
           }}
         >
-          <Typography
-            variant="subtitle2"
+          <Box
             sx={{
-              fontWeight: 700,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              mb: 1,
+            }}
+          >
+            <Typography
+              variant="subtitle2"
+              sx={{
+                fontWeight: 700,
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                color: "#333",
+                flexGrow: 1,
+              }}
+            >
+              {task.title} - {task.startTime} to {task.endTime}
+            </Typography>
+            <Box sx={{ display: "flex", gap: 1 }}>
+              <IconButton
+                size="small"
+                color="primary"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  openEdit(task);
+                }}
+              >
+                <EditIcon fontSize="small" />
+              </IconButton>
+              <IconButton
+                size="small"
+                color="error"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleDelete(task.id);
+                }}
+              >
+                <DeleteIcon fontSize="small" />
+              </IconButton>
+              <IconButton
+                size="small"
+                color="action"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  openDetails(task);
+                }}
+              >
+                <VisibilityIcon fontSize="small" />
+              </IconButton>
+            </Box>
+          </Box>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            sx={{
+              fontSize: "0.8rem",
               overflow: "hidden",
               textOverflow: "ellipsis",
               whiteSpace: "nowrap",
-              color: "#333",
-              flexGrow: 1,
             }}
           >
-            {task.title}
+            {task.description.length > 15
+              ? `${task.description.slice(0, 15)}...`
+              : task.description}
           </Typography>
-          <Box sx={{ display: "flex", gap: 1 }}>
-            <IconButton
-              size="small"
-              color="primary"
-              onClick={(e) => {
-                e.stopPropagation();
-                openEdit(task);
-              }}
-            >
-              <EditIcon fontSize="small" />
-            </IconButton>
-            <IconButton
-              size="small"
-              color="error"
-              onClick={(e) => {
-                e.stopPropagation();
-                handleDelete(task.id);
-              }}
-            >
-              <DeleteIcon fontSize="small" />
-            </IconButton>
-            <IconButton
-              size="small"
-              color="action"
-              onClick={(e) => {
-                e.stopPropagation();
-                openDetails(task);
-              }}
-            >
-              <VisibilityIcon fontSize="small" />
-            </IconButton>
-          </Box>
         </Box>
-        <Typography
-          variant="body2"
-          color="textSecondary"
-          sx={{
-            fontSize: "0.8rem",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            whiteSpace: "nowrap",
-          }}
-        >
-          {task.description}
-        </Typography>
-      </Box>
+      </Tooltip>
     );
   };
+
+  useEffect(() => {
+    const updateTimePosition = () => {
+      const now = new Date();
+      const hours = now.getHours();
+      const minutes = now.getMinutes();
+      const totalMinutes = hours * 60 + minutes;
+      const percentage = (totalMinutes / (24 * 60)) * 70;
+      setCurrentTimePosition(percentage);
+    };
+
+    updateTimePosition();
+    const intervalId = setInterval(updateTimePosition, 60000);
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <Box
@@ -307,6 +332,8 @@ function App() {
                     borderBottom:
                       idx < timeSlots.length - 1 ? "1px solid #f0f0f0" : "none",
                     backgroundColor: idx % 2 === 0 ? "#f5f5f5" : "transparent",
+                    borderRight: `1px solid #eee`,
+                    marginLeft: 2,
                   }}
                 >
                   <Typography
@@ -320,10 +347,16 @@ function App() {
             </Box>
             <Box sx={{ flexGrow: 1, position: "relative", padding: 1 }}>
               {tasks.map((task, index) => {
-                const [startHour, startMinute] = task.startTime.split(":").map(Number);
-                const [endHour, endMinute] = task.endTime.split(":").map(Number);
+                const [startHour, startMinute] = task.startTime
+                  .split(":")
+                  .map(Number);
+                const [endHour, endMinute] = task.endTime
+                  .split(":")
+                  .map(Number);
 
-                const isOvernight = endHour < startHour || (endHour === startHour && endMinute < startMinute);
+                const isOvernight =
+                  endHour < startHour ||
+                  (endHour === startHour && endMinute < startMinute);
 
                 if (isOvernight) {
                   const endOfDay = "23:59";
@@ -336,8 +369,13 @@ function App() {
                   const overlapStartOfDayEnd = task.endTime;
 
                   return (
-                    <>
-                      {renderTaskBlock(task, index, overlapEndOfDayStart, overlapEndOfDayEnd)}
+                    <React.Fragment key={`${task.id}-overnight-fragment`}>
+                      {renderTaskBlock(
+                        task,
+                        index,
+                        overlapEndOfDayStart,
+                        overlapEndOfDayEnd
+                      )}
                       <Box
                         key={`${task.id}-continued`}
                         className="task-block continued-task"
@@ -347,34 +385,103 @@ function App() {
                           right: 8,
                           background: taskColors[index % taskColors.length],
                           borderLeft: `4px solid ${
-                            taskColors[((index % taskColors.length) * 2) % taskColors.length]
+                            taskColors[
+                              ((index % taskColors.length) * 2) %
+                                taskColors.length
+                            ]
                           }`,
                           boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
                           padding: 1,
                           borderRadius: 4,
                           overflow: "hidden",
                           zIndex: 1,
-                          opacity: 0.6,
-                          fontStyle: 'italic',
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
+                          opacity: 0.75,
+                          fontStyle: "italic",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
                           top: 0,
-                          height: `calc((((${parseInt(overlapStartOfDayEnd.split(':')[0])} * 60 + ${parseInt(overlapStartOfDayEnd.split(':')[1])}) - (0 * 60 + 0)) / (24 * 60)) * 70vh)`,
-                          '&::before': {
+                          height: `calc((((${parseInt(
+                            overlapStartOfDayEnd.split(":")[0]
+                          )} * 60 + ${parseInt(
+                            overlapStartOfDayEnd.split(":")[1]
+                          )}) - (0 * 60 + 0)) / (24 * 60)) * 70vh)`,
+                          "&::before": {
                             content: '"(Continued)"',
-                            fontSize: '0.7rem',
-                            color: '#555',
-                          }
+                            fontSize: "0.7rem",
+                            color: "#555",
+                          },
                         }}
-                      >
-                      </Box>
-                    </>
+                      ></Box>
+                      <Box
+                        sx={{
+                          position: "absolute",
+                          left: 4,
+                          top: `calc((((${parseInt(
+                            overlapEndOfDayEnd.split(":")[0]
+                          )} * 60 + ${parseInt(
+                            overlapEndOfDayEnd.split(":")[1]
+                          )}) / (24 * 60)) * 70vh) - 8px)`,
+                          width: 0,
+                          height: 0,
+                          borderTop: "8px solid #aaa",
+                          borderLeft: "4px solid transparent",
+                          borderRight: "4px solid transparent",
+                          zIndex: 0,
+                        }}
+                      />
+                      <Box
+                        sx={{
+                          position: "absolute",
+                          left: 4,
+                          top: `calc((((${parseInt(
+                            overlapStartOfDayStart.split(":")[0]
+                          )} * 60 + ${parseInt(
+                            overlapStartOfDayStart.split(":")[1]
+                          )}) / (24 * 60)) * 70vh) + 2px)`,
+                          width: 0,
+                          height: 0,
+                          borderBottom: "8px solid #aaa",
+                          borderLeft: "4px solid transparent",
+                          borderRight: "4px solid transparent",
+                          zIndex: 0,
+                        }}
+                      />
+                    </React.Fragment>
                   );
                 } else {
-                  return renderTaskBlock(task, index, task.startTime, task.endTime);
+                  return renderTaskBlock(
+                    task,
+                    index,
+                    task.startTime,
+                    task.endTime
+                  );
                 }
               })}
+              {currentTimePosition !== null && (
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: `${currentTimePosition}vh`,
+                    left: 0,
+                    right: 0,
+                    height: "1px",
+                    backgroundColor: "red",
+                    zIndex: 2,
+                    "&::after": {
+                      content: '"Now"',
+                      position: "absolute",
+                      backgroundColor: "red",
+                      color: "white",
+                      fontSize: "0.7rem",
+                      padding: "2px 4px",
+                      borderRadius: "4px",
+                      top: "-8px",
+                      left: "105px",
+                    },
+                  }}
+                />
+              )}
             </Box>
           </Box>
         </Paper>
